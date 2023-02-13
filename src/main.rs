b717c824129f452e279c52c2f625bb46dbf349ef
Sungho Park <mrchaos@naver.com>
2023-02-13 21:49:00 +0900

    0213

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,20 @@
-// use std::fmt::format;
-use gcd::Gcd;
 
-use actix_web::{web, App, HttpResponse, HttpServer};
-use serde::{Deserialize};
 
-#[derive(Deserialize)]
-struct GcdParameters {
-    n: u64,
-    m: u64
+fn main(){
+    // 점검
+    assert_eq!(10_u8.checked_add(20), Some(30));
+    assert_eq!(100_u8.checked_add(200), None); 
+    // Some(300) 은 u8의 범위를 벗어나므로 에러 발생
+
+    // 순환
+    assert_eq!(100_u16.wrapping_mul(200), 20000);
+    assert_eq!(500_u16.wrapping_mul(500), 53392);
+    // 53392는 i16의 값을 벗어나므로 2의 16으로 나눈 나머지가 출력된다
+
+    // 포화
+    assert_eq!(32760_i16.saturating_add(10), 32767);
+    // assert_eq!();
+    // // 넘침
+    assert_eq!(255_u8.overflowing_add(2), (1, true));
+    // assert_eq!();
 }
-
-fn main() {
-    let server = HttpServer::new(|| {
-        App::new()
-                .route("/", web::get().to(get_index))
-                .route("/gcd", web::post().to(post_gcd))
-    });
-
-    println!("serving on http://localhost:3000...");
-    server.bind("127.0.0.1:3000")
-        .expect("error binding server to address")
-        .run().expect("error running server");
-}
-
-fn get_index() -> HttpResponse {
-    HttpResponse::Ok()
-                .content_type("text/html")
-                .body(r#"
-                        <title>GCD Calculator</title>
-                        <form action="/gcd" method="post">
-                            <input type="text" name="n"/>
-                            <input type="text" name="m"/>
-                            <button type="submit">Compute GCD</button>
-                        </form>
-                    "#,
-                )
-}
-
-fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
-    if form.n == 0 || form.m == 0 {
-        return HttpResponse::BadRequest()
-                            .content_type("text/html")
-                            .body("Computng the GCD with zero is boring");
-    }
-
-    let response = 
-        format!("The greatest common divisor if the numbers {} and {} is <b>{}</b>\n",
-                form.n, form.m, form.n.gcd(form.m));
-    
-    HttpResponse::Ok().content_type("text/html").body(response)
-}
-
-
